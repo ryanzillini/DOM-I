f10f1162afd3a8f488fc344169b24e18738634cf
Ryan Zillini <nyrtrading134@gmail.com>
2023-03-02 13:09:26 -0500

    completed module 1 project

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-const siteContent = { // DO NOT CHANGE THIS OBJECT
-  "nav": {
+const siteContent = {
+  // DO NOT CHANGE THIS OBJECT
+  nav: {
     "nav-item-1": "Services",
     "nav-item-2": "Product",
     "nav-item-3": "Vision",
@@ -7,36 +8,114 @@ const siteContent = { // DO NOT CHANGE THIS OBJECT
     "nav-item-5": "About",
     "nav-item-6": "Contact",
   },
-  "cta": {
-    "h1": "DOM Is Awesome",
-    "button": "Get Started",
+  cta: {
+    h1: "DOM Is Awesome",
+    button: "Get Started",
   },
   "main-content": {
     "features-h4": "Features",
-    "features-content": "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "features-content":
+      "Features content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "about-h4": "About",
-    "about-content": "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "about-content":
+      "About content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "services-h4": "Services",
-    "services-content": "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "services-content":
+      "Services content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "product-h4": "Product",
-    "product-content": "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "product-content":
+      "Product content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
     "vision-h4": "Vision",
-    "vision-content": "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
+    "vision-content":
+      "Vision content elementum magna eros, ac posuere elvit tempus et. Suspendisse vel tempus odio, in interdutm nisi. Suspendisse eu ornare nisl. Nullam convallis augue justo, at imperdiet metus scelerisque quis.",
   },
-  "contact": {
+  contact: {
     "contact-h4": "Contact",
-    "address": "123 Way 456 Street Somewhere, USA",
-    "phone": "1 (888) 888-8888",
-    "email": "sales@greatidea.io",
+    address: "123 Way 456 Street Somewhere, USA",
+    phone: "1 (888) 888-8888",
+    email: "sales@greatidea.io",
   },
-  "footer": {
-    "copyright": "Copyright Great Idea! 2021",
+  footer: {
+    copyright: "Copyright Great Idea! 2021",
   },
-  "images": {
+  images: {
     "logo-img": "http://localhost:9000/img/logo.png",
     "cta-img": "http://localhost:9000/img/cta.png",
     "accent-img": "http://localhost:9000/img/accent.png",
   },
 };
 
-console.log('project wired!')
+/* NAV */
+const navLinks = document.querySelectorAll("header nav a");
+const navLinkTexts = Object.values(siteContent.nav);
+navLinks.forEach((link, idx) => {
+  link.textContent = navLinkTexts[idx];
+  link.classList.add("italic");
+});
+
+/* CTA */
+const ctaText = document.querySelector(".cta-text");
+ctaText.children[0].textContent = siteContent.cta["h1"];
+ctaText.children[1].textContent = siteContent.cta["button"];
+
+/* MAIN-CONTENT */
+// TOP CONTENT
+const topContent = document.querySelector(".top-content");
+topContent.children[0].children[0].textContent =
+  siteContent["main-content"]["features-h4"];
+topContent.children[0].children[1].textContent =
+  siteContent["main-content"]["features-content"];
+
+topContent.children[1].children[0].textContent =
+  siteContent["main-content"]["about-h4"];
+topContent.children[1].children[1].textContent =
+  siteContent["main-content"]["about-content"];
+
+// BOTTOM CONTENT
+const bottomContent = document.querySelector(".bottom-content");
+bottomContent.children[0].children[0].textContent =
+  siteContent["main-content"]["services-h4"];
+bottomContent.children[0].children[1].textContent =
+  siteContent["main-content"]["services-content"];
+
+bottomContent.children[1].children[0].textContent =
+  siteContent["main-content"]["product-h4"];
+bottomContent.children[1].children[1].textContent =
+  siteContent["main-content"]["product-content"];
+
+bottomContent.children[2].children[0].textContent =
+  siteContent["main-content"]["vision-h4"];
+bottomContent.children[2].children[1].textContent =
+  siteContent["main-content"]["vision-content"];
+
+/* CONTACT */
+const contactInfo = document.querySelector(".contact");
+const contactTitle = contactInfo.querySelector("h4");
+contactTitle.textContent = "Contact";
+
+const contactP1 = contactInfo.querySelector("p");
+contactP1.textContent = "123 Way 456 Street Somewhere, USA";
+
+const contactP2 = contactP1.nextElementSibling;
+contactP2.textContent = "1 (888) 888-8888";
+
+const contactP3 = contactP2.nextElementSibling;
+contactP3.textContent = "sales@greatidea.io";
+
+/* FOOTER */
+const footerCopyright = document.querySelector("footer a");
+footerCopyright.href = "#";
+footerCopyright.textContent = "Copyright Great Idea! 2021";
+footerCopyright.classList.add("bold");
+
+/* IMAGES */
+const logoImg = document.querySelector("#logo-img");
+logoImg.src = siteContent.images["logo-img"];
+
+const ctaImg = document.querySelector("#cta-img");
+ctaImg.src = siteContent.images["cta-img"];
+
+const accentImg = document.querySelector("#middle-img");
+accentImg.src = siteContent.images["accent-img"];
+
+console.log("project wired!");
